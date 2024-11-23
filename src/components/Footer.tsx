288bd8b08bf33060e5159e2aa75a6fa41b85177e
cdcrypto <74511588+cdcrypto@users.noreply.github.com>
2024-11-23 15:45:46 -0400

    wrsedg
    rstged

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,10 +20,10 @@ export default function Footer() {
               </svg>
             </a>
             <a
-              href=""
+              href="https://t.me/+PAiSfBjNGJo5YWQx"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-gray-400 hover:text-yellow-500 transition-colors"
+              className="text-gray-400 hover:text-blue-500 transition-colors"
             >
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221l-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.446 1.394c-.14.18-.357.223-.548.223l.188-2.85 5.18-4.68c.223-.198-.054-.308-.346-.11l-6.4 4.02-2.76-.918c-.6-.183-.612-.6.125-.89l10.782-4.156c.5-.183.943.112.78.89z"/>
